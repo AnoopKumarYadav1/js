40d82bd88670102ed350399e18afe35500633879
AnoopKumarYadav1 <96728182+AnoopKumarYadav1@users.noreply.github.com>
2024-05-06 06:37:39 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// alert(3+3) // We are using nodejs not browser
+
+console.log("Abc"); console.log("cde") // Code readability should be high not like this statement
+
+console.log("Abc");
+console.log("cde")
+
+console.log(3
+    +3  // Code readability should be high not like this statement
+)
+
+console.log("Anoop")
+
+let  name="Anoop"
+let age=18
+let isLoggedIn=false;
+
+// number  max limit 2 power 53
+// bigint
+// string =>"" or ''
+// boolean => true/false
+// null => a standalone value
+// undefined => unassigned variable
+// symbol => to check uniqueness specially used in figma
+// The above datat types are primitive data types
+
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof "Anoop");
+console.log(typeof null); //object
+console.log(typeof undefined);// undefined
