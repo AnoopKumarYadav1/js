48d0202ccc24d4ace38d27be4a7edfdef13c3b07
Anoop Kumar Yadav <96728182+AnoopKumarYadav1@users.noreply.github.com>
2024-05-12 11:08:49 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -53,3 +53,44 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value5 = 3
+let negValue = -value5
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " anoop"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  output 12 
+// console.log(1 + "2");  output 12 
+// console.log("1" + 2 + 2);  output 122 
+// console.log(1 + 2 + "2"); output 32 
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); output 1 convert to number
+// console.log(true+); synatax error
+
+// console.log(+""); first convert "" to bool then +false
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
